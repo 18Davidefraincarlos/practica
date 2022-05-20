03a6d0c280fd1fc7386145248f1913e7793e5d1f
David Carlos <105508378+18Davidefraincarlos@users.noreply.github.com>
2022-05-20 16:28:34 -0300

    Update App.java
    Segundo mensaje custom

--- Practica/src/main/java/com/carlos/App.java
+++ Practica/src/main/java/com/carlos/App.java
@@ -11,6 +11,6 @@ public class App
     public static void main( String[] args )
     {
       //  System.out.println( "Hello World!" );
-    	JOptionPane.showMessageDialog(null, "Soy Elio");
+    	JOptionPane.showMessageDialog(null, "Soy Carlos");
     }
 }
