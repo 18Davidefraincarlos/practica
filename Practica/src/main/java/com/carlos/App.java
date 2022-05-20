74de5537dcdfa51e828c5772378cc72008cf386c
David Carlos <David Carlos@DavidEfrainCarlos>
2022-05-20 16:25:58 -0300

    Tu comentario custom

---
+++ Practica/src/main/java/com/carlos/App.java
@@ -0,0 +1,16 @@
+package com.carlos;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      //  System.out.println( "Hello World!" );
+    	JOptionPane.showMessageDialog(null, "Soy Elio");
+    }
+}
